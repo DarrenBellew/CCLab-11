9320d7d51e9dc702adb0e8bc9ca4d54195839fd0
User <islarf6546@gmail.com>
2015-11-25 18:53:11 +0200

    finished create queue

--- create-aws-queue.py
+++ create-aws-queue.py
@@ -1,5 +1,5 @@
-import simplejson, uuid, boto, requests
-
+import boto, requests
+import boto.sqs, sys
 
 def getKeyandId():
 	res = requests.get('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
@@ -11,5 +11,6 @@ def createQueue(conn, queueName):
 	print("queue '" + queueName + "'' is now created.")
 
 #Main Code
-conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
-createQueue(conn, "C13729611: " + argv[0])
+keyId, key = getKeyandId()
+conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=keyId, aws_secret_access_key=key)
+createQueue(conn, "C13729611_" + sys.argv[1])
