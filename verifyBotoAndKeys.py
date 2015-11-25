4215268c2b5120c1e9e96b88a5e6353f020ce3ee
User <islarf6546@gmail.com>
2015-11-25 18:02:35 +0200

    small bugfix

--- verifyBotoAndKeys.py
+++ verifyBotoAndKeys.py
@@ -1,7 +1,7 @@
 import requests
 res = requests.get('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
 output = res.text
-keyId, key = output.split(str=":")
+keyId, key = output.split(":")
 
 
 print(keyId + " | " + key)
