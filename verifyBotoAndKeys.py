7858dd1374d5a6469f6c5d93375d8df097f7d26e
User <islarf6546@gmail.com>
2015-11-25 18:06:34 +0200

    design, and added boto version line

--- verifyBotoAndKeys.py
+++ verifyBotoAndKeys.py
@@ -1,7 +1,11 @@
 import requests
+import boto
+
 res = requests.get('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
 output = res.text
 keyId, key = output.split(":")
 
 
-print(keyId + " | " + key)
+print(keyId)
+print(key)
+print(boto.Version)
