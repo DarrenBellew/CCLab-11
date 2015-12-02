a3d923485af1dace1a6daa2ae53d12544f6ade77
islarf <islarf6546@gmail.com>
2015-12-02 12:38:58 +0000

    Fixed write

--- write-aws-to-queue.py
+++ write-aws-to-queue.py
@@ -1,6 +1,6 @@
 import boto, requests
 import boto.sqs, sys
-
+from boto.sqs.message import message
 def getKeyandId():
 	res = requests.get('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
 	keyId, key = res.text.split(":")
